924151b5117546a15ddc3cc8b635f3a43663a0ed
Tereshchenko Vlad <ddiavall@yandex.ru>
2023-01-01 20:41:58 +0300

    Initial commit

---
+++ src/main/java/com/vlter/learnlangs/LearnLangsApplication.java
@@ -0,0 +1,13 @@
+package com.vlter.learnlangs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnLangsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnLangsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vlter/learnlangs/LearnLangsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlter.learnlangs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnLangsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
